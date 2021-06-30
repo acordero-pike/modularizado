256e4c37f7c6dc7636c96f6406cbcef2aef0fcee
acordero-pike <76223645+acordero-pike@users.noreply.github.com>
2021-06-30 17:22:59 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,5 @@
 import App  from './Clases/App.js'
-   import {ctsa}  from './Clases/Selectore.js'
-document.addEventListener('DOMContentLoaded', () => {
-            ctsa =JSON.parse( localStorage.getItem('citas') ) || []  ;
-        
-            if(ctsa.length >0)
-                {
-                    administrarCitas.cargardatos(ctsa);
-                    ui.imprimirCitas(administrarCitas)
-                }
-                 
-                    eventListeners();
-                
-        })
+   
+
 
 const app = new App();
