7410acca7aec94e35fca26fa433a899819ff0823
acordero-pike <76223645+acordero-pike@users.noreply.github.com>
2021-06-30 16:59:20 -0600

    Add files via upload

---
+++ js/Clases/App.js
@@ -0,0 +1,36 @@
+
+import {datosCita,nuevaCita} from '../Funciones.js';
+import {mascotaInput,propietarioInput,telefonoInput,fechaInput,horaInput,sintomasInput,formulario} from "./Selectore";
+
+class App {
+
+    cosntructor () {
+       this.appinit();
+    }
+
+    appinit()
+    {
+        mascotaInput.addEventListener('change', datosCita);
+        propietarioInput.addEventListener('change', datosCita);
+        telefonoInput.addEventListener('change', datosCita);
+        fechaInput.addEventListener('change', datosCita);
+        horaInput.addEventListener('change', datosCita);
+        sintomasInput.addEventListener('change', datosCita);
+        document.querySelector('#borrar').addEventListener('click',eliminarlocal)
+        formulario.addEventListener('submit', nuevaCita);
+        document.addEventListener('DOMContentLoaded', () => {
+            ctsa =JSON.parse( localStorage.getItem('citas') ) || []  ;
+        
+            if(ctsa.length >0)
+                {
+                    administrarCitas.cargardatos(ctsa);
+                    ui.imprimirCitas(administrarCitas)
+                }
+                 
+                    eventListeners();
+                
+        })
+    }
+}
+
+export default App;
